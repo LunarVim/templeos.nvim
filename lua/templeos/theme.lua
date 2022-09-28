5c160b295097e284a38237271d6b28ac66bfa86a
ChristianChiarulli <chris.machine@pm.me>
2022-09-28 14:43:34 -0400

    maybe this will help

--- lua/templeos/theme.lua
+++ lua/templeos/theme.lua
@@ -351,7 +351,7 @@ theme.set_highlights = function()
 
   -- StatusLine
   hl(0, "StatusLine", { fg = c.context, bg = c.bg })
-  hl(0, "StatusLineNC", { fg = c.line, bg = c.bg })
+  hl(0, "StatusLineNC", { fg = "NONE", bg = "NONE" })
   hl(0, "StatusLineSeparator", { fg = c.line, bg = 'NONE' })
   hl(0, "StatusLineTerm", { fg = c.line, bg = 'NONE' })
   hl(0, "StatusLineTermNC", { fg = c.line, bg = 'NONE' })
